7e70c3ed902e72972f1974e8b68481871586326c
rapphcd <77208630+rapphcd@users.noreply.github.com>
2026-02-08 11:50:58 +0100

    project start

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,47 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CleanupPlugin extends Plugin {
+	cleanedFiles = 0;
 
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		let ribbonIcon = this.addRibbonIcon("trash", "Clean medias", (e: MouseEvent) => {
+			this.clickOnRibbon(e, ribbonIcon)
+		})
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.registerEvent(this.app.vault.on('create', () => {
+			if (!ribbonIcon.isShown()) {
+				ribbonIcon = this.addRibbonIcon("trash", "Clean medias", (e: MouseEvent) => {
+					this.clickOnRibbon(e, ribbonIcon)
+				})
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
+		}))
+
+	}
+
+	clickOnRibbon(e: MouseEvent, icon: HTMLElement) {
+		const statusElement = this.addStatusBarItem().createEl("span")
+
+		this.cleanMediaFiles()
+		statusElement.setText(this.cleanedFiles.toString() + " File Cleaned")
+
+		icon.detach()
+
+		setTimeout(() => {
+			statusElement.detach()
+		}, 8000)
+	}
+
+
+	cleanMediaFiles() {
+		this.cleanedFiles = 0
+		const filesList = this.app.vault.getFiles();
+		for (const file of filesList) {
+			if (file.extension != "md") {
+				this.cleanedFiles += 1;
+				//clean
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-	}
-
-	onunload() {
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		}
 	}
 }
